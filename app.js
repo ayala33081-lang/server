5f506901c4ed0016f8b3b1186f594372ca644943
Ayala <ayala33081@gmail.com>
2026-07-15 22:14:20 +0300

    make a fun that update a book

--- app.js
+++ app.js
@@ -21,6 +21,31 @@ app.get('/books/:id', (req, res) => {
   }
 });
 
+app.post('/books',(req,res)=>{
+    if(req?.body){
+        books.push(req.body);
+        res.status(201).json(req.body);
+    }
+    else{
+        res.status(409).json({ error: 'books is required' });
+    }
+});
+
+app.put('/books/:id',(req,res)=>{
+    const book = books.find(b => b.code === +(req.params.id));
+    if(!book){
+        res.status(409).json({error:"id didnt found!"}); 
+    }
+    else{
+    book.category=req.body.category;
+    book.name=req.body.name;
+    book.price=req.body.price;
+    book.isBorrowed=false;
+    books.push(book);
+    res.status(201).json(books);
+    }
+});
+
 app.listen(5000, () => {
     console.log('Server is running on http://localhost:5000');
 });
