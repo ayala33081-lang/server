2667cda5aacaa92678df266af96afc09d13c4288
Ayala <ayala33081@gmail.com>
2026-07-16 10:01:22 +0300

    create data base of users

---
+++ users.js
@@ -0,0 +1,25 @@
+const users = [
+  {
+    code: 101,
+    username: 'ayaleD',
+    email: 'ayaleD@example.com',
+    password: '123456',
+    borrowedBookCodes: [1, 3]
+  },
+  {
+    code: 102,
+    username: 'noa',
+    email: 'noa@example.com',
+    password: 'abc123',
+    borrowedBookCodes: [2]
+  },
+  {
+    code: 103,
+    username: 'mam',
+    email: 'mam@example.com',
+    password: 'pass789',
+    borrowedBookCodes: []
+  }
+];
+
+export default users;
