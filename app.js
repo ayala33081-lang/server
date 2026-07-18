860062838db474bb6801258f3e774454e5123bd5
Ayala <ayala33081@gmail.com>
2026-07-18 23:50:18 +0300

    connect central index router to main app under /api

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import books from './db.js';
+import indexRouter from './routes/index.route.js';
 
 const app = express();
 app.use(express.json());
+app.use('/api',indexRouter);
 
 app.get('/', (req, res) => {
     res.status(200).json({message : 'Hello World'});
