356098b3a1e22ccaf3a52a4eb027d023a490d55e
Ayala <ayala33081@gmail.com>
2026-07-20 18:11:58 +0300

    add user validation schema using Joi

---
+++ controllers/validators/user.validator.js
@@ -0,0 +1,14 @@
+import Joi from 'joi';
+
+export const signUpSchema = Joi.object({
+    code: Joi.number().integer().required(),
+    username: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required()
+});
+
+export const signInSchema = Joi.object({
+    code: Joi.number().integer().required(),
+    username: Joi.string().required(),
+    password: Joi.string().required()
+});
