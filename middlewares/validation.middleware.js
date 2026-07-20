2ce3fec2aca17ef5ef06b02c9e77d86b24c5c971
Ayala <ayala33081@gmail.com>
2026-07-20 19:14:33 +0300

    refactor: improve error handling in validation middleware for sign up and sign in

--- middlewares/validation.middleware.js
+++ middlewares/validation.middleware.js
@@ -15,3 +15,30 @@ export const validateBook=(req,res,next)=>{
     next(err);
     }
 };
+
+export const validateSignUp=(req,res,next)=>{
+    try{
+    const {error} =signUpSchema.validate(req.body);
+    if(error){
+        const cusErr=new Error(error.details[0].message);
+        cusErr.status=400;
+        throw cusErr;
+    }
+    next();
+    }
+    catch(err){next(err);}
+};
+
+export const validateSignIn = (req, res, next) => {
+    try {
+        const { error } = signInSchema.validate(req.body);
+        if (error) {
+            const customError = new Error(error.details[0].message);
+            customError.status = 400;
+            throw customError;
+        }
+        next();
+    } catch (err) {
+        next(err);
+    }
+};
